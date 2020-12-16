f1a1fdfe44110cd56105a2770b9d7546aca132a9
Aditya <adityakumarmishra.2017@vitstudent.ac.in>
2020-12-16 12:01:02 +0530

    First Commit

---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+myAnswere = 42
+
+def galvins_quote():
+    print('I\'ll tell your chances of survival, but I don\'t think y\'ll like it')
+
+
+def square_root(x):
+    return math.sqrt(x)
